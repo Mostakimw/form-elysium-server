e133c253ff4d069e7b578d404cd3ff7a9c2c1022
Mostakimw <scientist.expert@gmail.com>
2023-10-30 23:35:11 +0600

    mongodb connected

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // middle ware or getting routes
+app.use(cors());
+app.use(express.json());
 const product_routes = require("./src/routes/products");
 
 app.get("/", (req, res) => {

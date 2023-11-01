061ff89e0d9d9d9725faa44f600f8232057cd5db
Mostakimw <scientist.expert@gmail.com>
2023-11-01 19:07:38 +0600

    specific form data api created

--- src/controllers/formDataController.js
+++ src/controllers/formDataController.js
@@ -1,5 +1,6 @@
 const connectToMongoDB = require("../config/db");
 
+// save form data to db
 const saveFormData = async (req, res) => {
   const client = await connectToMongoDB();
   const formCollection = client.db("formElysium").collection("forms");
@@ -10,8 +11,42 @@ const saveFormData = async (req, res) => {
     return res.status(200).send(result);
   } catch (err) {
     console.error("Error saving form data:", err);
-    res.status(500).json({ error: "An error occurred while saving form data" });
+    res.status(500).send({ error: "An error occurred while saving form data" });
   }
 };
 
-module.exports = { saveFormData };
+//get all form data from db
+const getFormData = async (req, res) => {
+  const userEmail = req.query.email;
+  const client = await connectToMongoDB();
+  const formCollection = client.db("formElysium").collection("forms");
+  try {
+    const result = await formCollection.find({ user: userEmail }).toArray();
+    return res.status(200).send(result);
+  } catch (err) {
+    console.error("Error saving form data:", err);
+    res
+      .status(500)
+      .send({ error: "An error occurred while getting form data" });
+  }
+};
+
+const getSpecificFormData = async (req, res) => {
+  const formId = req.query.formId;
+  const client = await connectToMongoDB();
+  const formCollection = client.db("formElysium").collection("forms");
+
+  try {
+    const result = await formCollection.findOne({ formId: formId });
+    if (!result) {
+      return res.status(404).send({ error: "Form data not found" });
+    }
+    return res.status(200).send(result);
+  } catch (err) {
+    console.error("Error getting specific form data:", err);
+    res
+      .status(500)
+      .send({ error: "An error occurred while getting specific form data" });
+  }
+};
+module.exports = { saveFormData, getFormData, getSpecificFormData };
--- src/routes/formDataRoutes.js
+++ src/routes/formDataRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const formDataController = require("./../controllers/formDataController");
 
 router.post("/saveFormData", formDataController.saveFormData);
+router.get("/getFormData", formDataController.getFormData);
+router.get("/getSpecificFormData", formDataController.getSpecificFormData);
 
 module.exports = router;

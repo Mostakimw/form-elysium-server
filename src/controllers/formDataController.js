8995f2741ee73e801c3a175cb22f72e2d3c038a5
Mostakimw <scientist.expert@gmail.com>
2023-11-02 15:25:10 +0600

    form delete api

--- src/controllers/formDataController.js
+++ src/controllers/formDataController.js
@@ -49,4 +49,29 @@ const getSpecificFormData = async (req, res) => {
       .send({ error: "An error occurred while getting specific form data" });
   }
 };
-module.exports = { saveFormData, getFormData, getSpecificFormData };
+
+const deleteFormData = async (req, res) => {
+  const formId = req.query.formId;
+  console.log(formId);
+  const client = await connectToMongoDB();
+  const formCollection = client.db("formElysium").collection("forms");
+
+  try {
+    const result = await formCollection.deleteOne({ formId: formId });
+    if (!result) {
+      return res.status(404).send({ error: "Form data not found" });
+    }
+    return res.status(200).send(result);
+  } catch (err) {
+    console.error("Error deleting specific form data:", err);
+    res
+      .status(500)
+      .send({ error: "An error occurred while deleting specific form data" });
+  }
+};
+module.exports = {
+  saveFormData,
+  getFormData,
+  getSpecificFormData,
+  deleteFormData,
+};
--- src/routes/formDataRoutes.js
+++ src/routes/formDataRoutes.js
@@ -5,5 +5,6 @@ const formDataController = require("./../controllers/formDataController");
 router.post("/saveFormData", formDataController.saveFormData);
 router.get("/getFormData", formDataController.getFormData);
 router.get("/getSpecificFormData", formDataController.getSpecificFormData);
+router.delete("/deleteFormData", formDataController.deleteFormData);
 
 module.exports = router;
